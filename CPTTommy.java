b6624c318a16fc40a09eb1f7df1fa424e527e85b
DreamChaser0719 <dream080719@gmail.com>
2025-05-27 17:26:43 -0400

    Update CPTTommy.java

--- CPTTommy.java
+++ CPTTommy.java
@@ -6,18 +6,23 @@ public class CPTTommy {
 		
 		int intMoney = 1000;
 		String strName;
-		int intCards [] [];
 		
-		intCards = new int [3][52];
+		int intDeck [][] = deck();
+	
+	}
+	public static int [][] deck() {
+		
+		int intCards [] [];
+		intCards = new int [52][3];
 		
 		int intCard = 1;
 		int intSuit = 1;
 		int intRow;
 		
 		for (intRow = 0; intRow < 52; intRow++) {
-			intCards[0][intRow] = intCard;
-			intCards[1][intRow] = intSuit;
-			intCards[2][intRow] = (int)(100*Math.random());
+			intCards[intRow][0] = intCard;
+			intCards[intRow][1] = intSuit;
+			intCards[intRow][2] = (int)(100*Math.random());
 			intCard++;
 			
 			if (intCard == 14) {
@@ -25,7 +30,6 @@ public class CPTTommy {
 				intSuit++;
 			}
 		}
-			
-			
+		return intCards;
 	}
 }
