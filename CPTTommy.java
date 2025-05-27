e71d86f6c1822308d024e608cf6f296e59f65940
DreamChaser0719 <dream080719@gmail.com>
2025-05-27 10:48:11 -0400

    Update CPTTommy.java

--- CPTTommy.java
+++ CPTTommy.java
@@ -3,5 +3,12 @@ import arc.*;
 public class CPTTommy {
 	public static void main(String[] args){
 		Console con = new Console();
+		
+		int intMoney = 1000;
+		String strName;
+		int intCards [] [];
+		
+		intCards = new int [52][3];
+		
 	}
 }
