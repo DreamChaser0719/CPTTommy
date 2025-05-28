7a9abfce147ece3f3dfc16ad97a67a8fa1f95fe8
DreamChaser0719 <dream080719@gmail.com>
2025-05-28 10:49:41 -0400

    Update MethodsFile.java

--- MethodsFile.java
+++ MethodsFile.java
@@ -26,20 +26,23 @@ public class MethodsFile {
 	
 	public static int [][] sort(int intDeck [][]) {
 
-		int intTemp [] = new int [2];
+		int intTemp [] = new int [3];
 		int intCount;
+		int intCount2;
 		
-		for (intCount = 0; intCount < 51; intCount++) {
-			if (intDeck [intCount][2] > intDeck [intCount+1][2]) {
-				intTemp [0] = intDeck [intCount][0];
-				intTemp [1] = intDeck [intCount][1];
-				intTemp [2] = intDeck [intCount][2];
-				intDeck [intCount][0] = intDeck [intCount+1][0];
-				intDeck [intCount][1] = intDeck [intCount+1][1];
-				intDeck [intCount][2] = intDeck [intCount+1][2];
-				intDeck [intCount+1][0] = intTemp [0];
-				intDeck [intCount+1][1] = intTemp [1];
-				intDeck [intCount+1][2] = intTemp [2];
+		for (intCount2 = 0; intCount2 < 52-1; intCount2++) {
+			for (intCount = 0; intCount < 52-1; intCount++) {
+				if (intDeck [intCount][2] > intDeck [intCount+1][2]) {
+					intTemp [0] = intDeck [intCount][0];
+					intTemp [1] = intDeck [intCount][1];
+					intTemp [2] = intDeck [intCount][2];
+					intDeck [intCount][0] = intDeck [intCount+1][0];
+					intDeck [intCount][1] = intDeck [intCount+1][1];
+					intDeck [intCount][2] = intDeck [intCount+1][2];
+					intDeck [intCount+1][0] = intTemp [0];
+					intDeck [intCount+1][1] = intTemp [1];
+					intDeck [intCount+1][2] = intTemp [2];
+				}
 			}
 		}
 		return intDeck;
