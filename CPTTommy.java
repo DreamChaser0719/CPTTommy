8024e33b4ba59a8e9edebca525e9cbc448d27d1b
DreamChaser0719 <dream080719@gmail.com>
2025-06-09 21:06:02 -0400

    Bugs Patching
    Optimized blackjack and 5 cards logic, some small logic imperfections still remain...

--- CPTTommy.java
+++ CPTTommy.java
@@ -4,7 +4,7 @@ public class CPTTommy {
 	public static void main(String[] args){
 		Console con = new Console();
 
-		int intMoney = 1000;
+		int intMoney;
 		int intBet = 0;
 		int intCount = 2;
 		int intHits = 0;
@@ -16,6 +16,8 @@ public class CPTTommy {
 		String strInput;
 		String strLBName;
 		String strLB [][];
+		boolean doubledDown;
+		boolean skipDealerTurn;
 
 		while (true) {
 			con.println("Blackjack");
@@ -35,12 +37,16 @@ public class CPTTommy {
 			} else if (chrInputHome == 'q' || chrInputHome == 'Q') {
 				con.closeConsole();
 			} else if (chrInputHome == 'p' || chrInputHome == 'P') {
-
+				
+				intMoney = 1000;
 				con.println("Enter your name:");
 				strName = con.readLine();
 
 				boolean keepPlaying = true;
 				while (keepPlaying && intMoney > 0) {
+					skipDealerTurn = false;
+					doubledDown = false;
+				
 					int intDeck [][] = MethodsFile.deck();
 					intDeck = MethodsFile.sort(intDeck);
 					int intPlayers [][] = new int [5][2];
@@ -69,10 +75,16 @@ public class CPTTommy {
 					con.println("The cards currently adds up to " + intSum);
 
 					if ((intPlayers[0][0] == 1 && intPlayers[1][0] >= 10) || (intPlayers[0][0] >= 10 && intPlayers[1][0] == 1)) {
-						con.println("BLACKJACK! 3X BET!");
-						intMoney = intMoney + intBet * 2;
-						continue;
+						con.println("BLACKJACK! You win 3x your bet!");
+						intMoney += intBet * 2;
+						skipDealerTurn = true;
+						intSum = 0;
+						intSumDealer = 0;
+						intCount = 2;
+						intHits = 0;
+						continue; // Skip rest of the loop, go to next round
 					}
+
 
 					if (intSum == 9 || intSum == 10 || intSum == 11) {
 						con.println("Do you wish to double down? (Y/N)");
@@ -84,70 +96,83 @@ public class CPTTommy {
 								intBet = intMoney;
 							}
 
-							intPlayers [2][0] = intDeck [3][0];
-							intPlayers [2][1] = intDeck [3][1];
+							intPlayers[2][0] = intDeck[3][0];
+							intPlayers[2][1] = intDeck[3][1];
 
 							intSum = MethodsFile.CalculateTotal(intPlayers);
-							con.println("The cards adds up to " + intSum);
+							con.println("The cards add up to " + intSum);
+
+							intCount = 3;
+							doubledDown = true;
 						}
 					}
+					
+					if (!doubledDown) {
+						while (intSum < 22 && intCount < 5) {
+							con.println("Hit or Stand? (H/S)");
+							char chrInputMain = con.getChar();
+							if (chrInputMain == 'h' || chrInputMain == 'H') {
+								
+								intPlayers[intCount][0] = intDeck[intCount+1][0];
+								intPlayers[intCount][1] = intDeck[intCount+1][1];
+								con.println("Your new card is:");
+								con.print(intPlayers[intCount][0] + " " + intPlayers[intCount][1]);
+								con.println();
+								intCount++;
+								intHits++;
 
-					while (intSum < 22) {
-						con.println("Hit or Stand? (H/S)");
-						char chrInputMain = con.getChar();
-						if (chrInputMain == 'h' || chrInputMain == 'H') {
-							intPlayers [intCount][0] = intDeck [intCount+1][0];
-							intPlayers [intCount][1] = intDeck [intCount+1][1];
-							con.println("Your new card is:");
-							con.print(intPlayers[intCount][0] + " " + intPlayers[intCount][1]);
-							con.println();
-							intSum = MethodsFile.CalculateTotal(intPlayers);
-							con.println("Your new sum is: " + intSum);
-							intCount++;
-							intHits++;
-						} else if (chrInputMain == 's' || chrInputMain == 'S' || intHits == 4) {
-							break;
+								intSum = MethodsFile.CalculateTotal(intPlayers);
+								con.println("Your new sum is: " + intSum);
+
+								if (intCount == 5 && intSum <= 21) {
+									con.println("You have 5 cards and didn’t bust!");
+									skipDealerTurn = true;
+									break;
+								}
+
+							} else if (chrInputMain == 's' || chrInputMain == 'S') {
+								break;
+							}
 						}
 					}
-
-					con.println("The dealer's cards are: ");
-					con.println(intDealer [0][0] + " " + intDealer [0][1]);
-					intSumDealer = MethodsFile.CalculateTotal(intDealer);
-					intHits = 1;
-
-					while (intSumDealer < 17 && intHits < 5) {
-						intDealer [intHits][0] = intDeck [intCount+1][0];
-						intDealer [intHits][1] = intDeck [intCount+1][1];
-						con.print(intDealer[intHits][0] + " " + intDealer[intHits][1]);
-						con.println();
+					
+					if (!skipDealerTurn) {
+						con.println("The dealer's cards are: ");
+						con.println(intDealer [0][0] + " " + intDealer [0][1]);
 						intSumDealer = MethodsFile.CalculateTotal(intDealer);
-						intHits++;
-						intCount++;
+						intHits = 1;
+
+						while (intSumDealer < 17 && intHits < 5) {
+							intDealer [intHits][0] = intDeck [intCount+1][0];
+							intDealer [intHits][1] = intDeck [intCount+1][1];
+							con.print(intDealer[intHits][0] + " " + intDealer[intHits][1]);
+							con.println();
+							intSumDealer = MethodsFile.CalculateTotal(intDealer);
+							intHits++;
+							intCount++;
+						}
+						con.println("Dealer's sum is: " + intSumDealer);
 					}
-
-					con.println("Dealer's sum is: " + intSumDealer);
-
+				
 					if (intSum > 21) {
 						con.println("You busted!");
-						intMoney = intMoney - intBet;
+						intMoney -= intBet;
 					} else if (intSumDealer > 21) {
 						con.println("Dealer busted!");
-						intMoney = intMoney + intBet;
-					} else if (intPlayers[4][0] != 0) {
-						con.println("You got 5 cards and did not bust! You got 3x bet!");
-						intMoney = intMoney + intBet * 2;
-					} else if (intDealer[4][0] != 0) {
-						con.println("Dealer got 5 cards and did not bust! You lost 3x bet!");
-						intMoney = intMoney - intBet * 2;
+						intMoney += intBet;
+					} else if (intDealer[4][0] != 0 && intSumDealer <= 21) {
+						con.println("Dealer got 5 cards and did not bust! You lost your bet.");
+						intMoney -= intBet;
 					} else if (intSum > intSumDealer) {
 						con.println("You won!");
-						intMoney = intMoney + intBet;
-					} else if (intSum < intSumDealer){
+						intMoney += intBet;
+					} else if (intSum < intSumDealer) {
 						con.println("You lost!");
-						intMoney = intMoney - intBet;
+						intMoney -= intBet;
 					} else {
 						con.println("Tied! Bet returned");
 					}
+
 
 					if (intMoney < 0) {
 						intMoney = 0;
